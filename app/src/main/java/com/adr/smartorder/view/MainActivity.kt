c95c9d2003449fb6295cb1e77f55cb70362bb270
Aditya Dwi Rachmantya <aditya.rachmantya@dana.id>
2020-06-21 19:03:23 +0700

    Implement bottom navigation view in main activity.

--- app/src/main/java/com/adr/smartorder/view/MainActivity.kt
+++ app/src/main/java/com/adr/smartorder/view/MainActivity.kt
@@ -1,11 +1,34 @@
-package com.adr.smartorder
+package com.adr.smartorder.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.adr.smartorder.R
+import com.adr.smartorder.view.fragment.ChatFragment
+import com.adr.smartorder.view.fragment.MainFragment
+import com.adr.smartorder.view.fragment.OrderFragment
+import com.adr.smartorder.view.fragment.ProductFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fl_container, MainFragment())
+            .commit()
+
+        btm_nav_main.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home_menu -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fl_container, MainFragment()).commit()
+                R.id.product_menu -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fl_container, ProductFragment()).commit()
+                R.id.order_menu -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fl_container, OrderFragment()).commit()
+                R.id.chat_menu -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fl_container, ChatFragment()).commit()
+            }
+            true
+        }
     }
 }
