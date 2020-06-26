eaf9d3c52c98a0fe055f2bc1680d48f0d41ae318
Aditya Dwi Rachmantya <aditya.rachmantya@dana.id>
2020-06-26 09:30:37 +0700

    Add background color for button.

--- app/src/main/java/com/adr/smartorder/view/LoginActivity.kt
+++ app/src/main/java/com/adr/smartorder/view/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.adr.smartorder.view
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
